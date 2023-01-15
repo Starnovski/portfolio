a076eef1fd6dd84c57ff4849fd6298bf6e7e12f7
Starnovski <44373053+Starnovski@users.noreply.github.com>
2023-01-15 15:47:24 +0100

    Update settings.py

--- portal/settings.py
+++ portal/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql,
 
         'NAME': 'django_portal',
 
